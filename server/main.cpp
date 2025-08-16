c2f63307bef8c50d6d0059e6e08a583f3fbe76b3
NamhyeonKo <glucosamin7@sju.ac.kr>
2025-08-16 13:29:35 +0900

    change file name

--- server/main.cpp
+++ server/main.cpp
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <thread>
 
 static void print_usage(void) {
     printf("echo-server:\n");
@@ -27,9 +28,33 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    int server_fd, new_socket;
+    int server_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
+
+    // 클라이언트 처리 함수
+    auto handle_client = [](int client_socket) {
+        while (1) {
+            char buffer[1024];
+            int bytes_read = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+            if (bytes_read == 0) {
+                printf("Client disconnected\n");
+                break;
+            } else if (bytes_read < 0) {
+                perror("recv failed");
+                break;
+            }
+            
+            buffer[bytes_read] = '\0';
+            printf("%s\n", buffer);
+            
+            if (send(client_socket, buffer, bytes_read, 0) < 0) {
+                perror("send failed");
+                break;
+            }
+        }
+        close(client_socket);
+    };
 
     /* 소켓 생성 (ipv4, tcp, protocol auto) */
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -42,45 +67,29 @@ int main(int argc, char* argv[]) {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_family = AF_INET;
     address.sin_port = htons(atoi(port));
+    
     if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0) {
         perror("bind failed");
         close(server_fd);
         return 1;
     }
 
-    /* 연결 요청 기다리기 */
     if (listen(server_fd, 3) < 0) {
         perror("listen failed");
         close(server_fd);
         return 1;
     }
 
-    /* 연결 요청 수락 */
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    if (new_socket < 0) {
-        perror("accept failed");
-        close(server_fd);
-        return 1;
-    }
-
-    /* 송수신 */
     while (1) {
-        char buffer[1024];
-        int bytes_read = recv(new_socket, buffer, sizeof(buffer), 0);
-
-        if (bytes_read == 0) {
-            printf("Client disconnected\n");
-            break;
-        } else if (bytes_read < 0) {
-            perror("recv failed");
-            break;
+        int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        if (new_socket < 0) {
+            perror("accept failed");
+            continue;
         }
-
-        printf("%s\n", buffer);
+        std::thread t(handle_client, new_socket);
+        t.detach();
     }
 
-    close(new_socket);
     close(server_fd);
-
     return 0;
 }
